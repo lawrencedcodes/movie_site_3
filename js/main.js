1e11f9b9ae15c8414772c7ecbb66e9749162cce8
Lawrence Lockhart <ldlockhart@gmail.com>
2019-05-16 11:21:34 -0500

    Add

--- js/main.js
+++ js/main.js
@@ -44,6 +44,25 @@ function getMovie() {
                 <img src="${movie.Poster}" class="thumbnail">
             </div>
             <div class="col-md-8">
+                <h2>${movie.Title}</h2>
+                <ul class="list-group">
+                    <li class="list-group-item"><strong>Genre</strong>${movie.Genre}</li>
+                    <li class="list-group-item"><strong>Released</strong>${movie.Released}</li>
+                    <li class="list-group-item"><strong>Rated</strong>${movie.Rated}</li>
+                    <li class="list-group-item"><strong>IMDB Rating</strong>${movie.imdbRating}</li>
+                    <li class="list-group-item"><strong>Director</strong>${movie.Director}</li>
+                    <li class="list-group-item"><strong>Writer</strong>${movie.Writer}</li>
+                    <li class="list-group-item"><strong>Actors</strong>${movie.Actors}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="well">
+                <h3>Plot</h3>
+                <p>${movie.Plot}</p>
+                <hr/>
+                <a href="http://imdb.com/title/${movie.ID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                <a href="index.html class="btn btn-default">Search Again</a>
             </div>
         </div>
      `;
